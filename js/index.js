5e14b4ebbc614451ba61dc97561303b8ed388660
a88795596 <1105103643@qq.com>
2018-03-30 16:53:18 +0800

    Android兼容完成

--- js/index.js
+++ js/index.js
@@ -2,10 +2,20 @@ var videoBox = $("#videoBox")[0];
 var close = $(".close")[0];
 var video = $("video")[0];
 var boxs = $("h3");
+var Mycanvas = $("#Mycanvas")[0];
+var cont = Mycanvas.getContext("2d");
+var flag = false;
+var wWidth = window.innerWidth;
+var wHeight = window.innerHeight;
 
-drawCancas(document, window.innerWidth, window.innerHeight);
+Mycanvas.width = wWidth;
+Mycanvas.height = wHeight;
+var img = new Image()
+img.src = "./img/start.gif";
+cont.drawImage(img, 0, 0, wWidth, wHeight)
 document.addEventListener("WeixinJSBridgeReady", function () {
   video.play();
+  drawCancas(document);
 }, false);
 
 //视频结束触发
@@ -14,7 +24,6 @@ video.addEventListener("ended", function () {
   $(".content").show();
   animation();
 })
-
 /**
  * 三个盒子动画
  */
@@ -33,47 +42,39 @@ function animation() {
   }, 3000, "linear");
 }
 
+//在canvas中进行描绘
+function draw(v, c, c2, w, h) {
+  if (v.ended || flag) {
+    cancelAnimationFrame(stop);
+    return false;
+  }
+  c.fillStyle = "#000";
+  c.fillRect(0,0,window.width,window.height);
+  c.drawImage(v, 0, 0, wWidth, wHeight);
+  var stop = requestAnimationFrame(function () {
+    draw(v, c, c2, wWidth, wHeight);
+  });
+}
 /**
  * 画布同步视频
  */
-function drawCancas(doc, cw, ch) {
-  var Mycanvas = $("#Mycanvas")[0]
-  var cont = Mycanvas.getContext("2d")
-  var Myvideo = $("#mvideo")[0]
-  Mycanvas.width = cw;
-  Mycanvas.height = ch;
+function drawCancas(doc) {
+  
+  var Myvideo = $("#mvideo")[0];
+  Mycanvas.width = wWidth;
+  Mycanvas.height = wHeight;
+
+  var cont2;
+  draw(Myvideo, cont, cont2, wWidth, wHeight);
   Myvideo.addEventListener("canplay", function () {
-    var cont2 = cerateCanvas(cw, ch).getContext("2d");
-    draw(this, cont, cont2, cw, ch);
+    $(".start").hide()
+    draw(this, cont, cont2, wWidth, wHeight);
   }, false)
-
-  //创建canvas
-  function cerateCanvas(w, h) {
-    var cr = doc.createElement("canvas");
-    cr.width = w;
-    cr.height = h;
-    return cr;
-  }
-
-  //在canvas中进行描绘
-  function draw(v, c, c2, w, h) {
-    if (v.paused || v.ended) {
-      cancelAnimationFrame(stop);
-      return false;
-    }
-    c2.drawImage(v, 0, 0, w, h);
-    var idata = c2.getImageData(0, 0, w, h);
-    // var data = idata.data;
-    c.putImageData(idata, 0, 0);
-    var stop = requestAnimationFrame(function () {
-      draw(v, c, c2, w, h);
-    });
-  }
 }
 
 //点击重置
 $(".reset").on("click", function () {
-  window.location.reload();
+  window.location.reload()
 })
 
 //点击分享
@@ -85,18 +86,34 @@ $(".mask").on("click", function () {
   $(this).hide()
 })
 
-$(window).on("load", function () {
-  video.play();
-})
 $(".show").on("click", function () {
   $(".erweima").fadeIn(200)
 })
+$(window).on("click",function (){
+  $(".start").hide();
 
+  video.play();
+
+  drawCancas(document, wWidth, wHeight);
+})
 window.addEventListener("orientationchange", function () {
-  if (window.innerWidth < window.innerHeight) {
+  if (wWidth < wHeight) {
     $(".tips").fadeIn(1000);
     setTimeout(() => {
       $(".tips").fadeOut(1000);
     }, 1200);
   }
 }, false);
+$(window).on("resize", function (){
+  wWidth = this.innerWidth;
+  wHeight = this.innerHeight;
+  drawCancas(document, wWidth, wHeight);
+})
+
+
+
+
+
+
+
+
